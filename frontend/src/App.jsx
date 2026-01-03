3ec95b2fc793a14691123c33b677e31fcbd4bb69
ankitapanda832-wq <ankitapanda832@gmail.com>
2026-01-03 22:18:57 +0530

    Implmented Home & Collection Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,14 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
     <Navbar />
+    <SearchBar />
 
     <Routes>
       <Route path='/' element={<Home/>} />
@@ -28,6 +31,7 @@ const App = () => {
       <Route path='/orders' element={<Orders/>} />
       
     </Routes>
+    <Footer/>
     </div>
   )
 }
