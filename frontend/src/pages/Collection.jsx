ba540232b3ae6c0a0bbc7c68a488f1ae3b7b16f4
ankitapanda832-wq <ankitapanda832@gmail.com>
2026-01-04 23:19:59 +0530

    i will work in collection part

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/Productitem';
 
 const Collection = () => {
 
-const {products} = useContext(ShopContext);
+const { products, search , showSearch } = useContext(ShopContext);
 const [showFilter,setShowFilter] = useState(false);
 const [filterProducts,setFilterProducts] = useState([]);
 const [category,setCategory] = useState([]);
@@ -37,6 +37,11 @@ const toggleSubCategory = (e) => {
 
 const applyFilter = () => {
   let productsCopy = products.slice();
+
+  if (showSearch && search) {
+    productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+
+  }
 
 if(category.length > 0){
   productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -72,7 +77,7 @@ switch(sortType){
 useEffect(()=>{
   applyFilter();
 
-},[category,subCategory])
+},[category,subCategory,search,showSearch])
 
 useEffect(()=>{
   sortProducts();
